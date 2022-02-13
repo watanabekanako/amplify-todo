aa8ece70122ff64b64e102614e06d0c7fc3b9046
阿部直也 <n.abe@gemcook.com>
2022-02-13 14:07:12 +0900

    スタイルの微調整

--- src/views/pages/Main.tsx
+++ src/views/pages/Main.tsx
@@ -1,11 +1,14 @@
+import { VStack } from '@chakra-ui/react';
 import React from 'react';
+import AddTodo from '../components/AddTodo';
 import TodoList from '../components/TodoList';
 
 const Main: React.VFC = () => {
   return (
-    <div>
+    <VStack spacing={4} align='stretch' p={8}>
       <TodoList />
-    </div>
+      <AddTodo />
+    </VStack>
   );
 };
 
